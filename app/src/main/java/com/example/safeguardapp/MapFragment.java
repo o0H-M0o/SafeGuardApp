fda0e1c4418a0fb756a45ee658f6e304f865d86e
SW <swen4153@gmail.com>
2023-12-01 10:56:39 +0800

    bottom nav

--- app/src/main/java/com/example/safeguardapp/MapFragment.java
+++ app/src/main/java/com/example/safeguardapp/MapFragment.java
@@ -1,19 +1,137 @@
 package com.example.safeguardapp;
 
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.Manifest;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.location.Location;
+import android.widget.*;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.*;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private final int FINE_PERMISSION_CODE = 1;
+    private GoogleMap myMap;
+    private SearchView mapSearchView;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        getLastLocation();
+
+        mapSearchView = view.findViewById(R.id.mapSearch);
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList= null;
+
+                if (location != null){
+
+                    Geocoder geocoder = new Geocoder(requireContext());
+
+                    try{
+                        addressList = geocoder.getFromLocationName(location,1 );
+                     } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (addressList != null && !addressList.isEmpty()) {
+                        Address address = addressList.get(0);
+                        LatLng latlng = new LatLng(address.getLatitude(), address.getLongitude());
+                        myMap.addMarker(new MarkerOptions().position(latlng).title(location));
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10));
+                    } else {
+                        // Handle the case where no address is found
+                        Toast.makeText(requireContext(), "Location not found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return view;
+    }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location!=null){
+                    currentLocation=location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+                    if (mapFragment == null) {
+                        mapFragment = SupportMapFragment.newInstance();
+                        getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+                    }
+
+                    mapFragment.getMapAsync(MapFragment.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap=googleMap;
+
+        LatLng myLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        myMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12));
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Log.d("MapFragment", "Permission denied: Displaying Toast");
+                Toast.makeText(requireActivity(), "Location permission is denied, please allow the permission", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
